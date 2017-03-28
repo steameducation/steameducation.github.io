e77c9bd5af2a9e4058c3c4e4a75a441c9c4d1aeb
Óscar Rodrigues <oscarmsrodrigues@gmail.com>
2017-03-28 10:41:00 +0200

    opens facebook in another window

--- footer.js
+++ footer.js
@@ -1,5 +1,5 @@
 document.write('\
 <footer>\
-  	<a class="socialIcons" href="mailto:steameducationspace@gmail.com" title="Send me an e-mail!"><img class="resize" src="pics/email-96.png"/></a><a class="socialIcons" href="https://www.facebook.com/AgentsOfChangeInEducation/" title="Find me on Facebook"><img class="resize" src="pics/facebook-96.png"/></a><a class="socialIcons" href="https://twitter.com/OscarSRodrigues" title="Follow me on Twitter"><img class="resize" src="pics/twitter-96.png"/></a><a class="socialIcons" href="https://instagram.com/oscar_rodrigues/" title="Follow me on Instagram"><img class="resize" src="pics/instagram-96.png"/></a>\
+  	<a class="socialIcons" href="mailto:steameducationspace@gmail.com" title="Send me an e-mail!"><img class="resize" src="pics/email-96.png"/></a><a target="_blank" class="socialIcons" href="https://www.facebook.com/AgentsOfChangeInEducation/" title="Find me on Facebook"><img class="resize" src="pics/facebook-96.png"/></a><a class="socialIcons" href="https://twitter.com/OscarSRodrigues" title="Follow me on Twitter"><img class="resize" src="pics/twitter-96.png"/></a><a class="socialIcons" href="https://instagram.com/oscar_rodrigues/" title="Follow me on Instagram"><img class="resize" src="pics/instagram-96.png"/></a>\
 </footer>\
 ');
