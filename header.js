6e583b0b22d08cc23c4bf27578cc8b7552a92f05
Óscar Rodrigues <oscarmsrodrigues@gmail.com>
2017-03-28 10:36:07 +0200

    proper links to pages

--- header.js
+++ header.js
@@ -4,9 +4,9 @@ document.write('\
     <ul style="text-align: center;">\
         <li><a href="index.html">Home</a></li>\
 	    <li><a href="about.html">About</a></li>\
-        <li><a href="cv.html">Resources</a></li>\
-        <li><a href="works.html">Learn</a></li>\
-        <li><a href="software.html">Play</a></li>\
+        <li><a href="resources.html">Resources</a></li>\
+        <li><a href="learn.html">Learn</a></li>\
+        <li><a href="play.html">Play</a></li>\
         <li><a href="news.html">News</a></li>\
     </ul>\
 </nav>\
