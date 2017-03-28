77da7abf902aa2f10374b0afb288336793570bfd
Óscar Rodrigues <oscarmsrodrigues@gmail.com>
2017-03-28 11:23:08 +0200

    adds proper references to social media

--- footer.js
+++ footer.js
@@ -1,5 +1,9 @@
 document.write('\
 <footer>\
-  	<a class="socialIcons" href="mailto:steameducationspace@gmail.com" title="Send me an e-mail!"><img class="resize" src="pics/email-96.png"/></a><a target="_blank" class="socialIcons" href="https://www.facebook.com/AgentsOfChangeInEducation/" title="Find me on Facebook"><img class="resize" src="pics/facebook-96.png"/></a><a class="socialIcons" href="https://twitter.com/OscarSRodrigues" title="Follow me on Twitter"><img class="resize" src="pics/twitter-96.png"/></a><a class="socialIcons" href="https://instagram.com/oscar_rodrigues/" title="Follow me on Instagram"><img class="resize" src="pics/instagram-96.png"/></a>\
+  	<a class="socialIcons" href="mailto:steameducationspace@gmail.com" title="Send me an e-mail!"><img class="resize" src="pics/email-96.png"/></a>\
+  	<a target="_blank" class="socialIcons" href="https://www.facebook.com/AgentsOfChangeInEducation/" title="Find us on Facebook"><img class="resize" src="pics/facebook-96.png"/></a>\
+  	<a class="socialIcons" href="https://twitter.com/steamspace1" title="Follow us on Twitter"><img class="resize" src="pics/twitter-96.png"/></a>\
+  	<a class="socialIcons" href="https://www.instagram.com/steamspace2017/" title="Follow us on Instagram"><img class="resize" src="pics/instagram-96.png"/></a>\
+  	<a class="socialIcons" href="https://www.youtube.com/channel/UCpOWlpZEGKp6ggS0oY4DJRg" title="Find us on YouTube!"><img class="resize" src="pics/yt-96.png"/></a>\
 </footer>\
 ');
