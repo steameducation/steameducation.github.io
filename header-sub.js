dcddc9a8131fb016f21fb658f68c0c1d7ba60375
steameducation <steameducationspace@gmail.com>
2019-03-29 12:35:51 +0000

    Update header links Geert
    Update header links on Geert page

---
+++ header-sub.js
@@ -0,0 +1,23 @@
+document.write('\
+\
+<nav>\
+    <ul style="text-align: center;">\
+        <li><a href="../index.html">Home</a></li>\
+	    <li> <div class="dropdown">\
+	    	<button class="dropbtn">About</button>\
+	    		<div class="dropdown-content">\
+	    			<a href="../space.html"> SPACE Project </a>\
+	    			<a href="../team.html">SPACE Team </a>\
+	    			<a href="../waso.html"> WASO </a>\
+	    			<a href="../partners.html"> SPACE Partners </a>\
+	    		</div>\
+	    	</div>\
+	    </li>\
+        <li><a href="../resources.html">Resources</a></li>\
+        <li><a href="../learn.html">Learn</a></li>\
+        <li><a href="../play.html">Play</a></li>\
+        <li><a href="../news.html">News</a></li>\
+    </ul>\
+</nav>\
+\
+');
