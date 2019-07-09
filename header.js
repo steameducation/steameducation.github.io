5277085da4d17522130834800d0e95575569debe
steameducation <steameducationspace@gmail.com>
2019-07-09 15:48:51 +0100

    Add dropdown menu for resources
    Adding in drop down menu for Resources section

--- header.js
+++ header.js
@@ -13,7 +13,16 @@ document.write('\
 	    		</div>\
 	    	</div>\
 	    </li>\
-        <li><a href="resources.html">Resources</a></li>\
+	    <li> <div class="dropdown">\
+	    	<button class="dropbtn">Resources</button>\
+	    		<div class="dropdown-content">\
+	    			<a href="resources.html">Neptune App</a>\
+	    			<a href="team.html">WASO Guidebook</a>\
+	    			<a href="waso.html">STEAM Repository</a>\
+	    			<a href="partners.html">WASO Pedagogy</a>\
+	    		</div>\
+	    	</div>\
+	    </li>\
         <li><a href="learn.html">Learn</a></li>\
         <li><a href="play.html">Play</a></li>\
         <li><a href="news.html">News</a></li>\
